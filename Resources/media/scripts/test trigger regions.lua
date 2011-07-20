d0804fccfbf5502e56ec456bb4924ec718568e88
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2011-07-20 22:00:50 +0000

    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@39 ba97e771-f6de-4074-9afc-763755853dd4

--- Resources/media/scripts/test trigger regions.lua
+++ Resources/media/scripts/test trigger regions.lua
@@ -6,7 +6,7 @@ print("Running test trigger regions.lua")
 -- this will not get run just by running this file - it will only run if we call it directly, which is what the event is for :D
 -- it DOES matter where this function is - it #MUST# be above where you hook it to an event, like we're doing here!
 function testfunction(triggerShape, otherShape, triggerFlags)
-	if Triggers.isEnterFlag(triggerFlags) then
+	if isTriggerEnterFlag(triggerFlags) then
 		print("enter")
 	else
 		print("leave")
@@ -15,5 +15,5 @@ function testfunction(triggerShape, otherShape, triggerFlags)
 end
 
 -- hook our function to the event
-Triggers.hookFunctionToTriggerArea("test trigger area", testfunction)
+hookFunctionToTriggerArea("test trigger area", testfunction)
 
--- Resources/media/scripts/test.lua
+++ Resources/media/scripts/test.lua
@@ -1,19 +1,11 @@
 -- first we tell lua about our c# program
 luanet.load_assembly("Lymph")
 
--- then we tell it what things we're going to be using
---levelman = luanet.import_type("Lymph.Lua.LevelManagerWrapper")
---triggerw = luanet.import_type("Lymph.Lua.TriggerWrapper")
-
 print("test.lua has been run")
 -- then we do stuff!
---print("lets start level 4")
---levelman.loadLevelByName("Vessel")
---LevelManagerWrapper.hookScriptToLevelLoadEvent("media/scripts/testevent.lua")
---LevelManagerWrapper.hookFunctionToLevelLoadEvent(testfunction)
 
 function testfunctionn(s1, s2, tf)
-	if Triggers.isEnterFlag(tf) then
+	if isTriggerEnterFlag(tf) then
 		print("enter")
 	else
 		print("leave")
@@ -21,8 +13,8 @@ function testfunctionn(s1, s2, tf)
 	print(s2.Actor.Name)
 end
 
-Triggers.hookScriptToTriggerArea("test trigger area", "media/scripts/testevent.lua")
-Triggers.hookFunctionToTriggerArea("test trigger area", testfunctionn)
+hookScriptToTriggerArea("test trigger area", "media/scripts/testevent.lua")
+hookFunctionToTriggerArea("test trigger area", testfunctionn)
 
 
 
