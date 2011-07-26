93d6978f59f14401cda6a5560c95a814ac781d53
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2011-07-26 22:20:19 +0000

    THIS IS INCREDIBLY SILLY
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@69 ba97e771-f6de-4074-9afc-763755853dd4

---
+++ Lymph/Handlers/FluctuatingGravityHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Mogre;
+using Mogre.PhysX;
+using Ponykart.Phys;
+using Math = System.Math;
+
+namespace Ponykart.Handlers {
+	/// <summary>
+	/// SILLINESS AT ITS FINEST
+	/// </summary>
+	public class FluctuatingGravityHandler : IDisposable {
+		Scene scene;
+
+		public FluctuatingGravityHandler() {
+			LKernel.Get<Root>().FrameStarted += FrameStarted;
+			scene = LKernel.Get<PhysXMain>().Scene;
+		}
+
+		double elapsed;
+		Vector3 vec = new Vector3(0, 0, 0);
+		bool FrameStarted(FrameEvent evt) {
+			if (scene == null || scene.IsDisposed || scene.Gravity == null) {
+				Dispose();
+				return true;
+			}
+			elapsed += evt.timeSinceLastFrame;
+			vec.y = (float)(Math.Sin(elapsed) * 40) - 10;
+			scene.Gravity = vec;
+
+			return true;
+		}
+
+		public void Dispose() {
+			LKernel.Get<Root>().FrameStarted -= FrameStarted;
+		}
+	}
+}
