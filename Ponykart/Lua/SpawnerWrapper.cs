3f0551a791f9b756a28fd8f153fa34336fe84b92
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2011-08-19 16:07:30 +0000

    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@143 ba97e771-f6de-4074-9afc-763755853dd4

--- Ponykart/Lua/SpawnerWrapper.cs
+++ Ponykart/Lua/SpawnerWrapper.cs
@@ -1,9 +1,9 @@
 ﻿using LuaNetInterface;
 using Mogre;
+using Ponykart.Actors;
 using Ponykart.Core;
 
-namespace Ponykart.Lua
-{
+namespace Ponykart.Lua {
 	//[LuaPackage("Spawner", "This is a wrapper for the Spawner. Use it to spawn game objects.")]
 	[LuaPackage(null, null)]
 	public class SpawnerWrapper {
@@ -12,18 +12,20 @@ namespace Ponykart.Lua
 			LKernel.Get<LuaMain>().RegisterLuaFunctions(this);
 		}
 
-		[LuaFunction("spawn", "Spawns a game object!", "string type - The class name of the thing you want to spawn.",
-			"string name - The name of the thing you want to spawn. Don't worry if this isn't unique, an ID will be added.",
+		[LuaFunction("spawn", "Spawns a game object! Returns null if it doesn't exist.",
+			"string type - The name of the .thing you want to spawn. Not case sensitive.",
 			"number posX - X spawn position", "number posY - Y spawn position", "number posZ - Z spawn position")]
-		public static void Spawn(string type, string name, float posX, float posY, float posZ) {
+		public static LThing Spawn(string type,  float posX, float posY, float posZ) {
 			Spawner spawner = LKernel.Get<Spawner>();
 			if (spawner != null) {
 				try {
-					spawner.Spawn(type, name, new Vector3(posX, posY, posZ));
-				} catch {
-					LKernel.Get<LuaMain>().Print("[Spawner Wrapper] ERROR: The specified type \"" + type + "\" was not found!");
+					return spawner.Spawn(type, type, new Vector3(posX, posY, posZ));
+				}
+				catch {
+					LKernel.Get<LuaMain>().Print("[SpawnerWrapper] ERROR: The specified type \"" + type + "\" was not found!");
 				}
 			}
+			return null;
 		}
 	}
 }
