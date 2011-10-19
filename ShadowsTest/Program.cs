607d3b38db44eef08ffe13f3c22828cac1035373
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2011-10-19 22:29:42 +0000

    got PSSM shadows working!
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@350 ba97e771-f6de-4074-9afc-763755853dd4

--- ShadowsTest/Program.cs
+++ ShadowsTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Mogre;
+using MOIS;
+using Vector3 = Mogre.Vector3;
 
 namespace ShadowsTest {
 	/// <summary>
@@ -14,6 +16,10 @@ namespace ShadowsTest {
 		Camera camera;
 		Viewport viewport;
 		SceneNode rotatingNode;
+		InputManager InputManager;
+		Keyboard InputKeyboard;
+		Light directionalLight, directionalLight2, pointLight, spotLight;
+		bool quit = false;
 
 		static void Main(string[] args) {
 			using (Program p = new Program()) {
@@ -36,16 +42,15 @@ namespace ShadowsTest {
 			window = root.Initialise(true, "shadow test");
 
 			sceneMgr = root.CreateSceneManager(SceneType.ST_GENERIC, "sceneMgr");
-			sceneMgr.AmbientLight = ColourValue.Black;
-			sceneMgr.ShadowTechnique = ShadowTechnique.SHADOWTYPE_TEXTURE_ADDITIVE;
-			sceneMgr.SetShadowTextureSettings(1024, 3);
-			//sceneMgr.ShowDebugShadows = true;
+			sceneMgr.AmbientLight = new ColourValue(0.3f, 0.3f, 0.3f);
+
 
 			camera = sceneMgr.CreateCamera("cam");
-			camera.Position = new Vector3(100, 100, 100);
+			camera.Position = new Vector3(12, 12, 12);
 			camera.LookAt(new Vector3(0, 0, 0));
-			camera.NearClipDistance = 1;
-			camera.FarClipDistance = 1000;
+			camera.SetAutoTracking(true, sceneMgr.RootSceneNode);
+			camera.NearClipDistance = 0.1f;
+			camera.FarClipDistance = 2000;
 
 			viewport = window.AddViewport(camera);
 			viewport.BackgroundColour = ColourValue.Black;
@@ -55,10 +60,23 @@ namespace ShadowsTest {
 
 			TextureManager.Singleton.DefaultNumMipmaps = 1;
 
-			root.FrameStarted += FrameStarted;
-			root.FrameEnded += FrameEnded;
-
 			CreateThings();
+
+			SetupShadows();
+
+			SetupInput();
+
+			root.FrameStarted += FrameStarted;
+
+			Console.WriteLine(
+@"
+
+
+
+Press 1, 2, 3, 4 to enable/disable lights, or Esc to quit.
+The red and blue textures have PSSM and self-shadowing enabled.
+The yellow one does not."
+			);
 		}
 
 		/// <summary>
@@ -79,49 +97,85 @@ namespace ShadowsTest {
 			}
 		}
 
+		void SetupShadows() {
+			sceneMgr.ShadowTechnique = ShadowTechnique.SHADOWTYPE_TEXTURE_ADDITIVE_INTEGRATED;
+
+			sceneMgr.SetShadowTextureCountPerLightType(Light.LightTypes.LT_DIRECTIONAL, 3);
+			sceneMgr.ShadowTextureCount = 3;
+			sceneMgr.SetShadowTextureConfig(0, 1024, 1024, PixelFormat.PF_FLOAT32_R);
+			sceneMgr.SetShadowTextureConfig(1, 512, 512, PixelFormat.PF_FLOAT32_R);
+			sceneMgr.SetShadowTextureConfig(2, 512, 512, PixelFormat.PF_FLOAT32_R);
+			sceneMgr.ShadowTextureSelfShadow = true;
+			sceneMgr.ShadowCasterRenderBackFaces = false;
+			sceneMgr.ShadowFarDistance = 150;
+			sceneMgr.SetShadowTextureCasterMaterial("PSSM/shadow_caster");
+			sceneMgr.SetShadowTextureFadeStart(0.3f);
+
+			PSSMShadowCameraSetup pssm = new PSSMShadowCameraSetup();
+			pssm.SplitPadding = 1f;
+			pssm.CalculateSplitPoints(3, 0.0000001f, sceneMgr.ShadowFarDistance);
+			pssm.SetOptimalAdjustFactor(0, 2);
+			pssm.SetOptimalAdjustFactor(1, 1);
+			pssm.SetOptimalAdjustFactor(2, 0.5f);
+			pssm.UseSimpleOptimalAdjust = false;
+
+			sceneMgr.SetShadowCameraSetup(new ShadowCameraSetupPtr(pssm));
+		}
+
 		/// <summary>
 		/// create a bunch of crap
 		/// </summary>
 		void CreateThings() {
 			// a directional light to cast shadows
-			Light light = sceneMgr.CreateLight("sun");
-			light.Type = Light.LightTypes.LT_DIRECTIONAL;
-			light.Direction = new Vector3(0.5f, -1, -0.2f);
-			light.Direction.Normalise();
-			light.DiffuseColour = new ColourValue(0.4f, 0.4f, 0.4f);
-			light.SpecularColour = new ColourValue(0.4f, 0.4f, 0.4f);
-			light.CastShadows = true;
+			directionalLight = sceneMgr.CreateLight("sun");
+			directionalLight.Type = Light.LightTypes.LT_DIRECTIONAL;
+			directionalLight.Direction = new Vector3(0.5f, -1, -0.2f);
+			directionalLight.Direction.Normalise();
+			directionalLight.DiffuseColour = ColourValue.Red;
+			directionalLight.SpecularColour = ColourValue.Red;
+			directionalLight.Position = new Vector3(0, 10, 0);
+			directionalLight.CastShadows = true;
+
+			directionalLight2 = sceneMgr.CreateLight("sun2");
+			directionalLight2.Type = Light.LightTypes.LT_DIRECTIONAL;
+			directionalLight2.Direction = new Vector3(-0.1f, -1, 0.1f);
+			directionalLight2.Direction.Normalise();
+			directionalLight2.DiffuseColour = new ColourValue(0.7f, 0.7f, 0.7f);
+			directionalLight2.SpecularColour = new ColourValue(0.7f, 0.7f, 0.7f);
+			directionalLight.Position = new Vector3(0, 10, 0);
+			directionalLight2.CastShadows = true;
 
 			// and a point light
-			Light pointLight = sceneMgr.CreateLight("pointLight");
+			pointLight = sceneMgr.CreateLight("pointLight");
 			pointLight.Type = Light.LightTypes.LT_POINT;
-			pointLight.Position = new Vector3(-30, 100, 30);
+			pointLight.Position = new Vector3(-3, 10, 3);
 			pointLight.DiffuseColour = ColourValue.Blue;
 			pointLight.SpecularColour = ColourValue.Blue;
 			pointLight.CastShadows = true;
 
 			// and a spotlight
-			Light spotLight = sceneMgr.CreateLight("spotLight");
+			spotLight = sceneMgr.CreateLight("spotLight");
 			spotLight.Type = Light.LightTypes.LT_SPOTLIGHT;
 			spotLight.DiffuseColour = ColourValue.Green;
 			spotLight.SpecularColour = ColourValue.Green;
 			spotLight.Direction = new Vector3(-1, -1, 0);
-			spotLight.Position = new Vector3(100, 100, 0);
+			spotLight.Position = new Vector3(10, 10, 0);
 			spotLight.SetSpotlightRange(new Degree(35), new Degree(50));
 			spotLight.CastShadows = true;
 
 			// a plane for the shadows to be cast on
 			Plane plane = new Plane(Vector3.UNIT_Y, 0);
-			MeshManager.Singleton.CreatePlane("ground", ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, plane, 1000, 1000, 100, 100, true, 1, 10, 10, Vector3.UNIT_Z);
+			MeshManager.Singleton.CreatePlane("ground", ResourceGroupManager.DEFAULT_RESOURCE_GROUP_NAME, plane,
+				100, 100, 10, 10, true, 1, 10, 10, Vector3.UNIT_Z);
 			Entity groundEnt = sceneMgr.CreateEntity("GroundEntity", "ground");
 			sceneMgr.RootSceneNode.CreateChildSceneNode().AttachObject(groundEnt);
 			groundEnt.SetMaterialName("brick");
 			groundEnt.CastShadows = false;
 
 			// and then some boxes that will cast the shadows
-			rotatingNode = CreateBox(new Vector3(0, 40, 0), "redbrick");
-			CreateBox(new Vector3(20, 11, 20), "bluebrick");
-			CreateBox(new Vector3(-10, 20, -30), "yellowbrick");
+			rotatingNode = CreateBox(new Vector3(0, 4, 0), "redbrick");
+			CreateBox(new Vector3(2, 1.1f, 2), "bluebrick");
+			CreateBox(new Vector3(-1, 2, -3), "yellowbrick");
 		}
 
 		SceneNode CreateBox(Vector3 pos, string material = "brick") {
@@ -130,10 +184,63 @@ namespace ShadowsTest {
 			ent.CastShadows = true;
 			boxNode.AttachObject(ent);
 			boxNode.Position = pos;
-			boxNode.SetScale(5, 5, 5);
 			ent.SetMaterialName(material);
 
 			return boxNode;
+		}
+
+		void SetupInput() {
+			ParamList pl = new ParamList();
+			IntPtr windowHnd;
+			window.GetCustomAttribute("WINDOW", out windowHnd); // window is your RenderWindow!
+			pl.Insert("WINDOW", windowHnd.ToString());
+
+			InputManager = InputManager.CreateInputSystem(pl);
+			InputKeyboard = (Keyboard) InputManager.CreateInputObject(MOIS.Type.OISKeyboard, true);
+
+			InputKeyboard.KeyPressed += new KeyListener.KeyPressedHandler(InputKeyboard_KeyPressed);
+		}
+
+		bool InputKeyboard_KeyPressed(KeyEvent arg) {
+			Console.WriteLine(arg.key);
+
+			switch (arg.key) {
+				case KeyCode.KC_ESCAPE:
+					quit = true;
+					break;
+				case KeyCode.KC_1:
+					directionalLight.Visible = !directionalLight.Visible;
+					break;
+				case KeyCode.KC_2:
+					pointLight.Visible = !pointLight.Visible;
+					break;
+				case KeyCode.KC_3:
+					spotLight.Visible = !spotLight.Visible;
+					break;
+				case KeyCode.KC_4:
+					directionalLight2.Visible = !directionalLight2.Visible;
+					break;
+				case KeyCode.KC_W:
+					camera.Position += new Vector3(1, 0, 0);
+					break;
+				case KeyCode.KC_S:
+					camera.Position -= new Vector3(1, 0, 0);
+					break;
+				case KeyCode.KC_A:
+					camera.Position += new Vector3(0, 0, 1);
+					break;
+				case KeyCode.KC_D:
+					camera.Position -= new Vector3(0, 0, 1);
+					break;
+				case KeyCode.KC_Q:
+					camera.Position += new Vector3(0, 1, 0);
+					break;
+				case KeyCode.KC_E:
+					camera.Position -= new Vector3(0, 1, 0);
+					break;
+			}
+
+			return !quit;
 		}
 
 		void Start() {
@@ -143,16 +250,15 @@ namespace ShadowsTest {
 		readonly Quaternion rotQuat = new Quaternion(new Degree(0.5f), Vector3.UNIT_Y);
 
 		bool FrameStarted(FrameEvent evt) {
-			if (window.IsClosed)
+			if (quit || window.IsClosed)
 				return false;
+
+			InputKeyboard.Capture();
 
 			// rotate our box
 			rotatingNode.Rotate(rotQuat);
 
-			return true;
-		}
-		bool FrameEnded(FrameEvent evt) {
-			return true;
+			return !quit;
 		}
 
 		public void Dispose() {
--- ShadowsTest/Properties/AssemblyInfo.cs
+++ ShadowsTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ShadowsTest")]
+[assembly: AssemblyTitle("little program for testing shadows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Lymph Framework")]
 [assembly: AssemblyProduct("ShadowsTest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Pyritie 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
