244b3e238839dcd4b033dd018f5b1e5824f013ac
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2011-12-12 23:14:00 +0000

    fixed this so there's a maximum pitch it will go to
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@443 ba97e771-f6de-4074-9afc-763755853dd4

--- Ponykart/Sound/EngineDroneHandler.cs
+++ Ponykart/Sound/EngineDroneHandler.cs
@@ -29,7 +29,7 @@ namespace Ponykart.Sound {
 			if (!Pauser.IsPaused) {
 
 				foreach (var pair in list) {
-					pair.second.PlaybackSpeed = (Math.Abs(pair.first.VehicleSpeed) * 0.005f) + 1;
+					pair.second.PlaybackSpeed = Math.Min((Math.Abs(pair.first.VehicleSpeed) * 0.005f) + 1, 2.25f);
 				}
 			}
 
