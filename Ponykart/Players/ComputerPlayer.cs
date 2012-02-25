2d1cf7de339f018ce15a801e522223752fd0b9a2
davidhaitch <davidhaitch@ba97e771-f6de-4074-9afc-763755853dd4>
2012-02-25 23:23:52 +0000

    This adds an AI that slowly drives in a circle. Take a look at ComputerPlayer.SteerTowards to see what's broken.
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@586 ba97e771-f6de-4074-9afc-763755853dd4

--- Ponykart/Players/ComputerPlayer.cs
+++ Ponykart/Players/ComputerPlayer.cs
@@ -1,10 +1,48 @@
-﻿using Ponykart.Levels;
+﻿using PonykartParsers;
+using Ponykart.Levels;
+using Mogre;
+using System;
+using System.Collections.Generic;
 
 namespace Ponykart.Players {
-    public class ComputerPlayer : Player {
+    public class ComputerPlayer : Player
+    {
+        private Player Human;
+        private LinkedList<Vector3> Waypoints;
+        public ComputerPlayer(LevelChangedEventArgs eventArgs, int id) : base(eventArgs, id)
+        {
+            Human = LKernel.GetG<PlayerManager>().Players[0];
+            LKernel.GetG<Root>().FrameEnded += FrameEnded;
+            //Waypoints.AddLast(new Vector3(100, 0, 100));
+        }
 
-        public ComputerPlayer(LevelChangedEventArgs eventArgs, int id) : base(eventArgs, id) {
+        bool FrameEnded(FrameEvent evt)
+        {
+            // use LKernel.GetG<LevelManager>().CurrentLevel.Definition.Get__Property() to retrieve your waypoints
+            Kart.TurnMultiplier = SteerTowards(new Vector3(10, 0, 10));
+            Kart.Acceleration = 0.05f;
+            return true;
+        }
 
+        private float SteerTowards(Vector3 target)
+        {
+            float steer = 0.0f;
+            Vector3 pos = NodePosition;
+            Vector3 facing = Kart.Body.Orientation.ZAxis;
+            Vector3 vecToTar = target - NodePosition;
+            // Math.ATan2 probably plays into the solution to this problem
+            if (vecToTar.x < 0.0)
+                steer = 0.5f;
+            if (vecToTar.x > 0.0)
+                steer = -0.5f;
+            return steer;
+        }
+
+        public override void Detach()
+        {
+            LKernel.GetG<Root>().FrameEnded -= FrameEnded;
+
+            base.Detach();
         }
 
         protected override void UseItem() {
