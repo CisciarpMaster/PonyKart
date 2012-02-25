adb809abba3041f2574469ce812361b1458b81c6
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2012-02-25 20:19:06 +0000

    added a check so it only does most of this stuff if the level type is Race and not Menu
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@583 ba97e771-f6de-4074-9afc-763755853dd4

--- Ponykart/Handlers/SceneEnvironmentHandler.cs
+++ Ponykart/Handlers/SceneEnvironmentHandler.cs
@@ -15,56 +15,61 @@ namespace Ponykart.Handlers {
 		}
 
 		void OnLevelPostLoad(LevelChangedEventArgs eventArgs) {
-			var sceneMgr = LKernel.GetG<SceneManager>();
-			var def = eventArgs.NewLevel.Definition;
+			if (eventArgs.NewLevel.Type == LevelType.Race) {
+				var sceneMgr = LKernel.GetG<SceneManager>();
+				var def = eventArgs.NewLevel.Definition;
 
-			// background color
-			LKernel.GetG<Viewport>().BackgroundColour = def.GetVectorProperty("Background", Vector3.UNIT_SCALE).ToColourValue();
+				// background color
+				LKernel.GetG<Viewport>().BackgroundColour = def.GetVectorProperty("Background", Vector3.UNIT_SCALE).ToColourValue();
 
-			// ambient
-			sceneMgr.AmbientLight = def.GetVectorProperty("Ambient", Vector3.UNIT_SCALE).ToColourValue();
+				// ambient
+				sceneMgr.AmbientLight = def.GetVectorProperty("Ambient", Vector3.UNIT_SCALE).ToColourValue();
 
-			// sunlight
-			Light light = sceneMgr.CreateLight("sun");
-			light.Type = Light.LightTypes.LT_DIRECTIONAL;
-			light.Direction = def.GetVectorProperty("SunlightDirection", new Vector3(0.1f, -1f, 0.1f));
-			light.Direction.Normalise();
-			light.DiffuseColour = def.GetVectorProperty("SunlightColour", Vector3.UNIT_SCALE).ToColourValue();
-			light.SpecularColour = def.GetVectorProperty("SunlightColour", Vector3.UNIT_SCALE).ToColourValue();
-			// cast shadows if we want some
-			if (Options.ShadowDetail != ShadowDetailOption.None)
-				light.CastShadows = true;
+				// sunlight
+				Light light = sceneMgr.CreateLight("sun");
+				light.Type = Light.LightTypes.LT_DIRECTIONAL;
+				light.Direction = def.GetVectorProperty("SunlightDirection", new Vector3(0.1f, -1f, 0.1f));
+				light.Direction.Normalise();
+				light.DiffuseColour = def.GetVectorProperty("SunlightColour", Vector3.UNIT_SCALE).ToColourValue();
+				light.SpecularColour = def.GetVectorProperty("SunlightColour", Vector3.UNIT_SCALE).ToColourValue();
+				// cast shadows if we want some
+				if (Options.ShadowDetail != ShadowDetailOption.None)
+					light.CastShadows = true;
 
-			// skybox
-			if (def.StringTokens.ContainsKey("skybox"))
-				sceneMgr.SetSkyBox(true, def.GetStringProperty("Skybox", null), 1995f);
+				// skybox
+				if (def.StringTokens.ContainsKey("skybox"))
+					sceneMgr.SetSkyBox(true, def.GetStringProperty("Skybox", null), 1995f);
 
-			// fog
-			FogMode mode = FogMode.FOG_NONE;
-			string sMode = def.GetStringProperty("FogType", "None");
+				// fog
+				FogMode mode = FogMode.FOG_NONE;
+				string sMode = def.GetStringProperty("FogType", "None");
 
-			if (sMode.Equals("None", StringComparison.InvariantCultureIgnoreCase))
-				mode = FogMode.FOG_NONE;
-			else if (sMode.Equals("Exp", StringComparison.InvariantCultureIgnoreCase))
-				mode = FogMode.FOG_EXP;
-			else if (sMode.Equals("Exp2", StringComparison.InvariantCultureIgnoreCase))
-				mode = FogMode.FOG_EXP2;
-			else if (sMode.Equals("Linear", StringComparison.InvariantCultureIgnoreCase))
-				mode = FogMode.FOG_LINEAR;
+				if (sMode.Equals("None", StringComparison.InvariantCultureIgnoreCase))
+					mode = FogMode.FOG_NONE;
+				else if (sMode.Equals("Exp", StringComparison.InvariantCultureIgnoreCase))
+					mode = FogMode.FOG_EXP;
+				else if (sMode.Equals("Exp2", StringComparison.InvariantCultureIgnoreCase))
+					mode = FogMode.FOG_EXP2;
+				else if (sMode.Equals("Linear", StringComparison.InvariantCultureIgnoreCase))
+					mode = FogMode.FOG_LINEAR;
 
-			if (mode != FogMode.FOG_NONE) {
-				sceneMgr.SetFog(
-					mode,
-					def.GetQuatProperty("FogColour", Quaternion.IDENTITY).ToColourValue(),
-					0.001f,
-					def.GetFloatProperty("FogStart", 100),
-					def.GetFloatProperty("FogEnd", 500));
-			}
+				if (mode != FogMode.FOG_NONE) {
+					sceneMgr.SetFog(
+						mode,
+						def.GetQuatProperty("FogColour", Quaternion.IDENTITY).ToColourValue(),
+						0.001f,
+						def.GetFloatProperty("FogStart", 100),
+						def.GetFloatProperty("FogEnd", 500));
+				}
 
 #if DEBUG
-			// make some axes
-			LKernel.GetG<Spawner>().Spawn("Axis", Vector3.ZERO);
+				// make some axes
+				LKernel.GetG<Spawner>().Spawn("Axis", Vector3.ZERO);
 #endif
+			}
+			else if (eventArgs.NewLevel.Type == LevelType.Menu) {
+				LKernel.GetG<SceneManager>().AmbientLight = new ColourValue(1, 1, 1);
+			}
 		}
 	}
 }
