7bd32ebefa20a6517e815f6f821742f2559738bb
davidhaitch <davidhaitch@ba97e771-f6de-4074-9afc-763755853dd4>
2012-02-26 18:34:40 +0000

    Various improvements to AI
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@603 ba97e771-f6de-4074-9afc-763755853dd4

--- Ponykart/Players/ComputerPlayer.cs
+++ Ponykart/Players/ComputerPlayer.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System;
 using Mogre;
 using Ponykart.Core;
 using Ponykart.Levels;
 
 namespace Ponykart.Players {
 	public class ComputerPlayer : Player {
-		static float DecelThreshold = 50 * 50; // 110 ^ 2
-		static float WaypointThreshold = 45 * 45; // 100 ^ 2
+		const float DecelThreshold = 50 * 50; // 110 ^ 2
+		const float WaypointThreshold = 45 * 45; // 100 ^ 2
+        const float wanderlust = 30;
 		private int loop = 0;
+        private Vector3 deviation;
 		private Player Human;
 		private int currWaypoint = 0;
+        private Random random;
 		private List<Vector3> Waypoints = new List<Vector3>();
 
 		//private StreamWriter outfile = new StreamWriter("waypoints.txt");
 
 		public ComputerPlayer(LevelChangedEventArgs eventArgs, int id) : base(eventArgs, id) {
 			Human = LKernel.GetG<PlayerManager>().MainPlayer;
-			
+			random = new Random(ID);
 			//Waypoints.Add(new Vector3(200, 0, 200));
 			//Waypoints.Add(new Vector3(200, 0, -200));
 			//Waypoints.Add(new Vector3(-200, 0, -200));
@@ -28,10 +32,18 @@ namespace Ponykart.Players {
 
 			string line;
 			string[] tempStr;
+            Vector3 tempVec;
 			while ((line = infile.ReadLine()) != null) {
 				tempStr = line.Split(' ');
-				Waypoints.Add(new Vector3(float.Parse(tempStr[0]), float.Parse(tempStr[1]), float.Parse(tempStr[2])));
+                tempVec = new Vector3(float.Parse(tempStr[0]), float.Parse(tempStr[1]), float.Parse(tempStr[2]));
+                deviation.x = (float)(random.NextDouble() * wanderlust*2) - wanderlust;
+                deviation.y = 0;
+                deviation.z = (float)(random.NextDouble() * wanderlust*2) - wanderlust;
+                tempVec += deviation;
+				Waypoints.Add(tempVec);
+                LKernel.GetG<Spawner>().Spawn("Waypoint", tempVec);
 			}
+
 			infile.Close();
 
 			LKernel.GetG<Root>().FrameEnded += FrameEnded;
@@ -54,14 +66,15 @@ namespace Ponykart.Players {
 					// it's better to use the squared length because this way we avoid having to so a square root operation, which are pretty expensive
 					float distToTar = vecToTar.SquaredLength;
 					// I changed this so we only have to do the subtraction once
-					Kart.TurnMultiplier = SteerTowards(target, vecToTar);
-					
-					if (distToTar > DecelThreshold)
-						Kart.Acceleration = 0.5f;
-					else
-						Kart.Acceleration = 0.1f;
+                    
+                    float steerFactor = SteerTowards(target, vecToTar);
 
-					if (distToTar < WaypointThreshold) {
+					Kart.TurnMultiplier = steerFactor;
+                    Kart.Acceleration = 1.0f - System.Math.Abs(steerFactor);
+
+					if (distToTar < WaypointThreshold)
+                    {
+                        
 						currWaypoint++;
 						currWaypoint = currWaypoint % Waypoints.Count;
 					}
