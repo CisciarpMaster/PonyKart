29c4fc7b15a9f741844da6fafb1e78f89fec5fda
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2012-03-21 03:16:09 +0000

    slowed down SAA and fixed twi's suspension
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@654 ba97e771-f6de-4074-9afc-763755853dd4

--- Resources/media/scripts/levels/SweetAppleAcres/init.lua
+++ Resources/media/scripts/levels/SweetAppleAcres/init.lua
@@ -1,6 +1,12 @@
+local function decreaseKartSpeed(kart, id)
+	kart.MaxSpeed = kart.MaxSpeed / 1.4
+end
+
 function SweetAppleAcres(level)
 	create2DSound("SAA Ambience.ogg", true)
 	
 	create3DSound("Mountain Stream1.ogg", vector(280.454, 0, -210.054), true)
 	create3DSound("Mountain Stream1.ogg", vector(-100.915, 40.8328, -693.665), true)
+	
+	forEachKart(decreaseKartSpeed)
 end
