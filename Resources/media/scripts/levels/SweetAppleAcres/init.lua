18986bf589a596a14c98434a5820300ed7e6d424
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2012-03-23 23:33:40 +0000

    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@678 ba97e771-f6de-4074-9afc-763755853dd4

--- Resources/media/scripts/levels/SweetAppleAcres/init.lua
+++ Resources/media/scripts/levels/SweetAppleAcres/init.lua
@@ -5,8 +5,8 @@ end
 function SweetAppleAcres(level)
 	create2DSound("SAA Ambience.ogg", true)
 	
-	create3DSound("Mountain Stream1.ogg", vector(280.454, 0, -210.054), true)
-	create3DSound("Mountain Stream1.ogg", vector(-100.915, 40.8328, -693.665), true)
+	create3DSound("Mountain Stream1.ogg", vector(56.0908, 0, -42.0108), true)
+	create3DSound("Mountain Stream1.ogg", vector(-20.1831, 8.16656, -138.733), true)
 	
 	forEachKart(decreaseKartSpeed)
 end
