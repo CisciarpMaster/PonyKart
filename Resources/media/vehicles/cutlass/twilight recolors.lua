fabb36c3dfc9b928ac07a8d9fa7cdca9468b0897
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2012-05-27 21:22:32 +0000

    newwwww twilight kart! hooray
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@752 ba97e771-f6de-4074-9afc-763755853dd4

--- Resources/media/vehicles/cutlass/twilight recolors.lua
+++ Resources/media/vehicles/cutlass/twilight recolors.lua
@@ -1,6 +1,6 @@
 local function recolor(kart, id)
 	-- don't do this for player 0
-	if id ~= 0 and kart.Player.Character ~= "Applejack" then
+	if id ~= 0 and kart.Player.Character ~= "Applejack" and kart.Player.Character ~= "Rainbow Dash" then
 		setMaterial(kart, "TwiCutlass_" .. id)
 		--[[if kart.Player.Character == "Twilight Sparkle" then
 			setOneSubMaterial(kart.Driver, 0, 0, "TwilightBody_" .. id)
