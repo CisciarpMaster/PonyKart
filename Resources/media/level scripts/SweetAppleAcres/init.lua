a722adf0ebc580cfbc2f5029d24511c0df7ead9d
davidhaitch <davidhaitch@ba97e771-f6de-4074-9afc-763755853dd4>
2012-06-19 02:49:50 +0000

    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@767 ba97e771-f6de-4074-9afc-763755853dd4

--- Resources/media/level scripts/SweetAppleAcres/init.lua
+++ Resources/media/level scripts/SweetAppleAcres/init.lua
@@ -1,16 +1,16 @@
 local function decreaseKartSpeed(kart, id)
-	kart.MaxSpeed = kart.MaxSpeed / 1.4
-end
-
-function SweetAppleAcres(level)
-	create2DSound("SAA Ambience.ogg", true)
-	
-	create3DSound("Mountain Stream1.ogg", vector(56.0908, 0, -42.0108), true)
-	create3DSound("Mountain Stream1.ogg", vector(-20.1831, 8.16656, -138.733), true)
-	
-	forEachKart(decreaseKartSpeed)
-	
-	derpy = spawnDerpy(vector(0, 0, 0))
-	derpy:ChangeAnimation("Hover1")
-	derpy:AttachToKart(vector(-1, 1, 2), playerKart())
+ 	kart.MaxSpeed = kart.MaxSpeed / 1.4
+ 	end
+ 	
+ 	function SweetAppleAcres(level)
+ 	create2DSound("SAA Ambience.ogg", true)
+ 	
+ 	create3DSound("Mountain Stream1.ogg", vector(56.0908, 0, -42.0108), true)
+ 	create3DSound("Mountain Stream1.ogg", vector(-20.1831, 8.16656, -138.733), true)
+ 	
+ 	forEachKart(decreaseKartSpeed)
+ 	
+ 	--derpy = spawnDerpy(vector(0, 0, 0))
+ 	--derpy:ChangeAnimation("Hover1")
+ 	--derpy:AttachToKart(vector(-1, 1, -2), playerKart())
 end
