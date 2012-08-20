66f5ebd49eeaf635e721403453d441667c377fa3
pyritie <pyritie@ba97e771-f6de-4074-9afc-763755853dd4>
2012-08-20 02:49:27 +0000

    fixing these because the compiler wasn't sure which one to use (thanks for the heads up, @DevinXXS)
    git-svn-id: svn://svn.code.sf.net/p/ponykart/code/trunk@823 ba97e771-f6de-4074-9afc-763755853dd4

--- Build-Debug.cmd
+++ Build-Debug.cmd
@@ -1,3 +1,4 @@
 @echo off
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /p:Configuration=Debug
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /p:Configuration=Debug Ponykart.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /p:Configuration=Debug "Ponykart tools.sln"
 pause
--- Build-Release.cmd
+++ Build-Release.cmd
@@ -1,3 +1,4 @@
 @echo off
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /p:Configuration=Release Ponykart.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /p:Configuration=Release "Ponykart tools.sln"
 pause
